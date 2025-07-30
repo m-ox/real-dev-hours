2f2a8f45a16a884a1184226525498623f7a99185
Maudlin Oxalis <maudlin.oxalis@gmail.com>
2025-07-30 03:27:25 -0600

    added silly warnings

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,9 @@ export function enableRealDevHours(config = {}) {
       (el as HTMLElement).style.transform = `translate(${Math.random() * 3}px, ${Math.random() * 3}px)`;
     });
   }, 5000);
+
+  setInterval(() => {
+    console.warn("Failed to load resource: net::ERR_FAILED");
+    console.error("Uncaught Error: Too many re-renders. React limits the number of renders to prevent an infinite loop.");
+  }, 8000);
 }
